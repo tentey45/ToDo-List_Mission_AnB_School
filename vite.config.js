a8cbb0a094bb9b90de4dc75fb99b1ca496952ea0
tentey45 <cponhvorntey@gmail.com>
2025-12-14 03:54:29 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import React from 'react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [ tailwindcss(),],
+
+})
